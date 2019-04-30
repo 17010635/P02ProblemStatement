69b9b5d1b2a9336e38e84f01066c8b2ce51feb88
17010180 <17010180@myrp.edu.sg>
2019-04-30 15:14:25 +0800

    commit

--- app/src/main/java/sg/edu/rp/c346/p02_problemstatement/Holiday.java
+++ app/src/main/java/sg/edu/rp/c346/p02_problemstatement/Holiday.java
@@ -2,15 +2,15 @@ package sg.edu.rp.c346.p02_problemstatement;
 
 public class Holiday {
     private String holiday;
-    private boolean image;
+    private boolean icon;
     public Holiday(String holiday, boolean image){
         this.holiday = holiday;
-        this.image = image;
+        this.icon = image;
     }
     public String getHoliday(){
         return holiday;
     }
     public boolean isImage(){
-        return image;
+        return icon;
     }
 }
